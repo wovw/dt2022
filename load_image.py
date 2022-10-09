1f56cce8eedbc2d6caee11014894c6bf8925beb4
Krishnan Vellore <vskrishnan11@gmail.com>
2022-10-09 11:01:00 -0500

    fixed load

--- load_image.py
+++ load_image.py
@@ -2,10 +2,17 @@ from comic_images import detect_text
 from comic_autocorrect import fix_sentence
 import os
 import json
+from gtts import gTTS
+
+file_formats = [".jpg", ".gif", ".png", ".jpeg"]
 
 if __name__ == "__main__":
     path = 'IO/'
-    filename = os.listdir(path)[0]
+    filename = ""
+    dir_list = os.listdir(path)
+    for file in dir_list:
+        if os.path.splitext(path + file)[1].lower() in file_formats:
+            filename = file
 
     google_api_text = detect_text(path + filename)
     autocorrected_text = fix_sentence(google_api_text)
@@ -28,3 +35,6 @@ if __name__ == "__main__":
 
     with open(path + "json_file.json", "w") as file1:
         file1.write(json.dumps(json_output, sort_keys=True, indent=4 * ' '))
+
+    myobj = gTTS(text=(" ".join(autocorrected_text)), lang='en', slow=False)
+    myobj.save(path + "output.mp3")
